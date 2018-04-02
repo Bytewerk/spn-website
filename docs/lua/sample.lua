5429fdd2f1817eb5f0ac68463ae41a51a57de148
nachtgold <zimmermann.k@gmail.com>
2018-04-02 18:16:21 +0200

    lua dependencies added

--- docs/lua/sample.lua
+++ docs/lua/sample.lua
@@ -1,14 +1,14 @@
---- Define special sequences of characters.
--- For each pair (find, subs), the function will create a field named with
--- find which has the value of subs.
--- It also creates an index for the table, according to the order of insertion.
--- @param subs The replacement pattern.
--- @param find The pattern to find.
-function def_escapes (find, subs)
-   local special = { t = "\t", n = "\n", ['"'] = '"', ['\\'] = '\\', }
-   find = gsub (find, "\\(.)", function (x) return %special[x] or x end)
-   subs = gsub (subs, "\\(.)", function (x) return %special[x] or x end)
-   escape_sequences.n = escape_sequences.n+1
-   escape_sequences[escape_sequences.n] = find
-   escape_sequences[find] = subs
+--- This is a brief overview for all usable functions within the Bot logic.
+
+x = 1
+
+--- Main cycle
+-- Run is executed each frame.
+-- @param current_heading first parameter
+function run(current_heading)
+    x = 2 * x
+    for n=1,x do
+        current_heading = current_heading + api:get_rotation_speed()
+    end
+    return current_heading
 end
