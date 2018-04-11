d84455893fa500b8018ee6dc7c4b5d7b1893e78a
nachtgold <zimmermann.k@gmail.com>
2018-04-11 19:16:41 +0200

    project cleaned

--- highscore/views.py
+++ highscore/views.py
@@ -5,6 +5,3 @@ from core.models import SnakeGame
 def table(request):
     data = SnakeGame.objects.values('user__username').annotate(score=Max('score')).order_by('-score')
     return render(request, 'highscore/table.html', context={'highscores': data})
-
-
-
