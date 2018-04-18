1a854ffbc9d165226416e48600ef31a0dc45d783
nachtgold <zimmermann.k@gmail.com>
2018-04-18 22:26:59 +0200

    REST API implementation tested by unit tests - was implemented by design

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from core.models import SnakeVersion, ActiveSnake
 
-# Create your tests here.
+class RestTestCase(TestCase):
+    
+    def setUp(self):
+        self.user = 'lion'
+        self.client.force_login(User.objects.get_or_create(username=self.user)[0])
+
+    def test_list_snakes(self):
+        """List all snaken through the REST API"""
+        response = self.client.post('/snake/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No snakes defined yet.')
+
+    def test_new_snake_editor(self):
+        """Open new snake editor"""
+        response = self.client.post('/snake/create')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<textarea id="code" name="code"></textarea>')
+    
+    def test_create_snake(self):
+        """Create new snake"""
+        self.client.post('/snake/create', {'code': 'test code'})
+        self.assertEqual(len(SnakeVersion.objects.all()), 1)
+        self.client.post('/snake/create', {'code': 'test code'})
+        self.assertEqual(len(SnakeVersion.objects.all()), 2)
+    
+    def test_activate_snake(self):
+        """Activate a snake"""
+        self.client.post('/snake/create', {'code': 'test code'})
+        self.assertEqual(len(ActiveSnake.objects.all()), 0)
+
+        response = self.client.post('/snake/activate/{}'.format(1), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.json()['message'], 'Snake 1 was activated')
+        self.assertEqual(len(ActiveSnake.objects.all()), 1)
+
+    def test_delete_snake(self):
+        """Delete a snake"""
+        self.client.post('/snake/create', {'code': 'test code'})
+        self.assertEqual(len(SnakeVersion.objects.all()), 1)
+
+        self.client.post('/snake/delete/{}'.format(1))
+        self.assertEqual(len(SnakeVersion.objects.all()), 0)
