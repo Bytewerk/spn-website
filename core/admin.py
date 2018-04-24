6a21f0d3a2c916f1a1836a009c3e305c78949259
Hubert Denkmair <hubert@denkmair.de>
2018-04-24 22:04:46 +0200

    add server command support

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from core.models import SnakeGame
+from core.models import SnakeGame, ServerCommand
 
 admin.site.register(SnakeGame)
+
+
+class ServerCommandAdmin(admin.ModelAdmin):
+    list_display = ('user', 'dt_created', 'dt_processed', 'command', 'result', 'result_msg')
+
+admin.site.register(ServerCommand, ServerCommandAdmin)
