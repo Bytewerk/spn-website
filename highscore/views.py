7b015579a1b21ccb250c2f12e43b935a78c16f52
Hubert Denkmair <hubert@denkmair.de>
2018-04-25 19:03:47 +0200

    adjust model for gameserver

--- highscore/views.py
+++ highscore/views.py
@@ -3,5 +3,5 @@ from django.db.models import Max
 from core.models import SnakeGame
 
 def table(request):
-    data = SnakeGame.objects.values('user__username').annotate(score=Max('score')).order_by('-score')
+    data = SnakeGame.objects.values('user__username').annotate(score=Max('final_mass')).order_by('-final_mass')
     return render(request, 'highscore/table.html', context={'highscores': data})
