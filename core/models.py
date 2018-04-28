c1cf6c0ef174227e4db8110eb4c9fbc4b89183d3
Hubert Denkmair <hubert@denkmair.de>
2018-04-28 23:07:41 +0200

    remove teams from model

--- core/models.py
+++ core/models.py
@@ -3,18 +3,8 @@ from django.utils.timezone import now
 from django.contrib.auth.models import User
 
 
-class Team(models.Model):
-    leader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=255)
-
-
-class TeamMembership(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    request_date = models.DateTimeField(default=now, blank=True)
-    start_date = models.DateTimeField(null=True, blank=True)
-    end_date = models.DateTimeField(null=True, blank=True)
-
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class SnakeVersion(models.Model):
     class Meta:
@@ -50,11 +40,6 @@ class SnakeGame(models.Model):
 
     def __str__(self):
         return self.user.username + " " + str(self.start_date)
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    current_team = models.ForeignKey(Team, blank=True, null=True, on_delete=models.SET_NULL)
 
 
 class ServerCommand(models.Model):
