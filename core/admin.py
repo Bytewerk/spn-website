13f0eb9b877394be8d3df16c87969aadf5869080
Hubert Denkmair <hubert@denkmair.de>
2018-04-29 10:10:05 +0200

    implement viewer key / begin log handling

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from core.models import SnakeVersion, ActiveSnake, SnakeGame, ServerCommand
+from core.models import SnakeVersion, ActiveSnake, SnakeGame, ServerCommand, UserProfile
 
 admin.site.register(SnakeVersion)
 admin.site.register(ActiveSnake)
 admin.site.register(SnakeGame)
-
+admin.site.register(UserProfile)
 
 class ServerCommandAdmin(admin.ModelAdmin):
     list_display = ('user', 'dt_created', 'dt_processed', 'command', 'result', 'result_msg')
