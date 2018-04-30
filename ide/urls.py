b69b064ef362a9642ead510fea3937820c7bb64d
Hubert Denkmair <hubert@denkmair.de>
2018-04-30 23:42:33 +0200

    cleanup, make a ajax save function

--- ide/urls.py
+++ ide/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from . import views as core_views
+from . import views
 
 urlpatterns = [
-    path('', core_views.snake_list, name='snake'),
-    path('create', core_views.snake_create, name='snake_create'),
-    path('delete/<int:snake_id>', core_views.snake_delete, name='snake_delete'),
-    path('edit/<int:snake_id>', core_views.snake_edit, name='snake_edit'),
-    path('activate/<int:snake_id>', core_views.snake_activate, name='snake_activate'),
-    path('disable', core_views.snake_disable, name='snake_disable'),
-    path('restart', core_views.snake_restart, name='snake_restart'),
+    path('', views.snake_list, name='snake'),
+    path('create', views.snake_create, name='snake_create'),
+    path('delete/<int:snake_id>', views.snake_delete, name='snake_delete'),
+    path('edit/latest', views.snake_edit_latest, name='snake_edit_latest'),
+    path('edit/<int:snake_id>', views.snake_edit, name='snake_edit_version'),
+    path('activate/<int:snake_id>', views.snake_activate, name='snake_activate'),
+    path('disable', views.snake_disable, name='snake_disable'),
+    path('restart', views.snake_restart, name='snake_restart'),
+    path('edit/save', views.snake_save, name='snake_save'),
 ]
