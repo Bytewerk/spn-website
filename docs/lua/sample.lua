eaf536b49bc1ddd3dee80bc97832fb281e39a6ad
nachtgold <zimmermann.k@gmail.com>
2018-05-02 18:58:16 +0200

    lua doc memory hint added

--- docs/lua/sample.lua
+++ docs/lua/sample.lua
@@ -1,4 +1,7 @@
 --- This is a brief overview of all usable functions within the bot logic.
+--- Every bot is allowed to store 100MB within the global namespace and the 
+--- data persists until the bot dies. So the bot can save data from frame 
+--- to frame.
 
 --- That method is called each frame.
 -- Here you can implement you bot logic. The return value must be an radiant angle.
