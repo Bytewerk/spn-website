124d2b53a2adc2898616572e30e21b810892c192
Hubert Denkmair <hubert@denkmair.de>
2018-05-09 14:29:57 +0200

    relax password restrictions

--- Programmierspiel/settings.py
+++ Programmierspiel/settings.py
@@ -74,16 +74,13 @@ WSGI_APPLICATION = 'Programmierspiel.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 5,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
