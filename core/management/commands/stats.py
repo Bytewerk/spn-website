34e82b3c3d721124bd60ffae8ccac68d07094c54
Hubert Denkmair <hubert@denkmair.de>
2018-05-13 12:33:29 +0200

    add stats command

---
+++ core/management/commands/stats.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
+from core.models import SnakeGame, SnakeVersion
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = "show stats"
+
+    def handle(self, *args, **options):
+        num_games = SnakeGame.objects.all().count()
+
+        print("total number of games: ", num_games)
+        print()
+
+        print("ten players with the most versions:")
+        ten_most_versions = SnakeVersion.objects.values('user__username').annotate(num_versions=Count('id')).order_by('-num_versions')[:10]
+        for v in ten_most_versions:
+            print(v['num_versions'], v['user__username'])
+        print()
+
+        # most kills
+        kills = []
+        deaths = []
+        for user in User.objects.all():
+            kills.append([SnakeGame.objects.filter(killer=user).count(), user.username])
+            deaths.append([SnakeGame.objects.filter(user=user).count(), user.username])
+
+        print("ten players with the most kills:")
+        for k in sorted(kills, key=lambda v: v[0], reverse=True)[:10]:
+            print(k[0], ": ", k[1])
+        print()
+
+        print("ten players killed most often:")
+        for k in sorted(deaths, key=lambda v: v[0], reverse=True)[:10]:
+            print(k[0], ": ", k[1])
+        print()
